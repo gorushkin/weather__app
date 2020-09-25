929a6ffaf68b4459ee399f93ca720229f5faa525
gorushkin <gorushkin@gmail.com>
2020-09-25 21:24:19 +0300

    Переделал оформление

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -16,14 +16,21 @@ class TextInput extends React.Component {
     const { handler } = this.props;
 
     return (
-      <form onSubmit={handler(value)}>
-        <input
-          className="form-control"
-          onChange={this.onChangeHandler}
-          type="text"
-          name="city"
-          value={value}
-        />
+      <form className="form-row" onSubmit={handler(value)}>
+        <div className="col">
+          <input
+            className="form-control form-control-lg w-100"
+            onChange={this.onChangeHandler}
+            type="text"
+            name="city"
+            value={value}
+          />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-primary px-sm-5" type="submit">
+            Send
+          </button>
+        </div>
       </form>
     );
   }
